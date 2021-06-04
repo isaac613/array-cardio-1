5e57b5a802696533dad66cc4888622faa50d4a9e
Isaac <isaacseligcoding@gmail.com>
2021-06-04 18:59:10 +0100

    fifth challenge

--- script.js
+++ script.js
@@ -93,3 +93,11 @@ const totalYears = inventors.reduce((total, inventor) => {
 }, 0);
 
 console.log(totalYears);
+
+// 5. Sort the inventors by years lived
+const oldest = inventors.sort(function (a, b) {
+  const lastInventor = a.passed - a.year;
+  const nextInventor = b.passed - b.year;
+  return lastInventor > nextInventor ? -1 : 1;
+});
+console.table(oldest);
